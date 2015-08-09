dab39c4d27434fbdee459a47db11cc9f5cb2c337
Touko Keski-Sämpi <ankkamiess@gmail.com>
2015-08-09 11:20:30 +0300

    Added towers and build mode.

--- src/js/data/game.js
+++ src/js/data/game.js
@@ -6,6 +6,8 @@ module.exports = function(game) {
     { 
       id: 'btn_return',
       spritesheet: 'assets/ui/buttons/btn_placeholder.png',
+      // Over, out, down, up
+      frames: [0, 1, 2, 3],
       size: { 
         x: 100, 
         y: 50 
@@ -16,6 +18,28 @@ module.exports = function(game) {
       },
       actionOnClick: function() {
         game.state.start('stages');
+      }
+    },
+    { 
+      id: 'btn_buildmode',
+      spritesheet: 'assets/ui/buttons/btn_buildmode.png',
+      frames: [null, null],
+      size: { 
+        x: 100, 
+        y: 50 
+      },
+      pos: { 
+        x: 1040, 
+        y: 200 
+      },
+      actionOnClick: function() {
+        game.buildMode = !game.buildMode;
+        // Change build mode button frame to match build state
+        if (game.buildMode) {
+          this.frame = 1;
+        } else {
+          this.frame = 0;
+        }
       }
     }],
 
--- src/js/states/game.js
+++ src/js/states/game.js
@@ -6,14 +6,17 @@ module.exports = function () {
   return {
     data: {},
     map: {},
-    buttons: [],
+    buttons: {},
     images: [],
     layers: [],
     wave: 0,
     path: {x: [], y: []},
     lives: 10,
+    cursor: {},
+    buildMode: false,
 
     init: function() {
+      // Initialize variables
       this.images = [];
       this.layers = [];
       this.path.x = [];
@@ -22,20 +25,24 @@ module.exports = function () {
     },
 
     preload: function() {
-      // Load data from file
+      // Require data
       this.data = require('../data/game.js')(this);
 
       // Preload tile data
       this.load.tilemap('map', 'assets/game/maps/test.json', null, Phaser.Tilemap.TILED_JSON);
       this.load.image('test', 'assets/game/tiles/test.png');
+
+      // Test graphics
       this.load.image('enemy', 'assets/game/enemies/enemy.png');
+      this.load.image('tower', 'assets/game/towers/tower.png');
 
       // Preload button sprites from data
       for (var i = 0; i < this.data.buttons.length; i++) {
         this.load.spritesheet(this.data.buttons[i].id, 
                               this.data.buttons[i].spritesheet, 
                               this.data.buttons[i].size.x, 
-                              this.data.buttons[i].size.y, 4);
+                              this.data.buttons[i].size.y,
+                              this.data.buttons[i].frames.length);
       }
 
       // Preload image sprites from data
@@ -56,21 +63,23 @@ module.exports = function () {
       this.map = this.add.tilemap('map');
       this.map.addTilesetImage('test');
 
+      // Create background layer
       this.layers[0] = this.map.createLayer('Background');
       this.layers[0].resizeWorld();
 
       // Get path from object layer
-      this.calculateRoute(this.map.objects.Path[0].polyline,
-                          this.map.objects.Path[0].x,
-                          this.map.objects.Path[0].y);
+      this.calculateRoute();
 
       // Create buttons from data
       for (var i = 0; i < this.data.buttons.length; i++) {
         this.buttons[this.data.buttons[i].id] = this.add.button(this.data.buttons[i].pos.x, 
                                                                 this.data.buttons[i].pos.y, 
                                                                 this.data.buttons[i].id, 
-                                                                this.data.buttons[i].actionOnClick, 
-                                                                this, 0, 1, 2, 3);
+                                                                this.data.buttons[i].actionOnClick, null,
+                                                                this.data.buttons[i].frames[0],
+                                                                this.data.buttons[i].frames[1],
+                                                                this.data.buttons[i].frames[2],
+                                                                this.data.buttons[i].frames[3]);
       }
 
       // Create images from data
@@ -80,23 +89,65 @@ module.exports = function () {
                                                               this.data.images[i].id);
       }
 
+      // Create cursor rectangle
+      this.cursor = this.add.graphics();
+      this.cursor.lineStyle(2, 0x000000, 1);
+      this.cursor.beginFill(0xB0B0B0, 0.5);
+      this.cursor.drawRect(0, 0, 32, 32);
+      this.cursor.endFill();
+
       // Start first wave
       this.createNextWave();
+
+      // Add input event to create tower when clicking
+      this.input.onDown.add(function(pointer, event) { 
+        this.createTower(); 
+      }, this);
     },
 
     update: function() {
       this.game.debug.text("Lives: " + this.lives, 50, 50);
 
+      // Only show cursor when build mode is active
+      if (this.buildMode) {
+        // Get tile from game area
+        this.cursor.tile = this.map.getTile(this.layers[0].getTileX(this.input.activePointer.worldX),
+                                            this.layers[0].getTileY(this.input.activePointer.worldY));
+
+        // Update cursor position if tile was found
+        if (this.cursor.tile !== null) {
+          this.cursor.visible = true;
+          this.cursor.x = this.layers[0].getTileX(this.input.activePointer.worldX) * 32;
+          this.cursor.y = this.layers[0].getTileY(this.input.activePointer.worldY) * 32;
+          // Tint the cursor green or red
+          if (this.cursor.tile.properties.buildable) {
+            this.cursor.tint = 0x00FF00;
+          } else {
+            this.cursor.tint = 0xFF0000;
+          }       
+        } else {
+          // Hide cursor when not over game area
+          this.cursor.visible = false;
+        }
+      } else {
+        this.cursor.visible = false;
+      }
+
+      // If lives reach zero, game over
       if (this.lives === 0) {
         this.state.start('stages');
       }
+
     },
 
-    calculateRoute: function(pathArray, pathPosX, pathPosY) {
+    calculateRoute: function() {
+      // Get path from map
+      var pathArray = this.map.objects.Path[0].polyline;
+
       // Fix coordinates and convert path to an array that tweens support
       for (var i = 0;i < pathArray.length; i++) {
-        this.path.x.push(pathArray[i][0] + pathPosX); 
-        this.path.y.push(pathArray[i][1] + pathPosY); 
+        this.path.x.push(pathArray[i][0] + this.map.objects.Path[0].x); 
+        this.path.y.push(pathArray[i][1] + this.map.objects.Path[0].y); 
       }
     },
 
@@ -119,19 +170,31 @@ module.exports = function () {
       twns.push(this.add.tween(enemy).to({ x: this.path.x[1], y: this.path.y[1] }, duration, Phaser.Easing.Linear.None, true));
 
 
-      // Add rest of the tweens (if any)
+      // Add rest of the tweens and chain them (if any)
       for (var i = 2; i < this.path.x.length; i++) {
         duration = 5 * this.math.distance(this.path.x[i-1], this.path.y[i-1], this.path.x[i], this.path.y[i]);
         twns.push(this.add.tween(enemy).to({ x: this.path.x[i], y: this.path.y[i] }, duration, Phaser.Easing.Linear.None));
         twns[twns.length - 2].chain(twns[twns.length - 1]);
       }
 
-      // If last tween finishes, destroy enemy and reduce lives
+      // If last tween finishes, destroy enemy sprite and reduce lives
       twns[twns.length - 1].onComplete.add(function() { 
         enemy.destroy(); 
         this.lives -= 1; 
       }, this);
 
+    },
+
+    createTower: function() {
+      // Check if trying to build on a tile
+      if (this.cursor.tile) {
+        // Build a tower if possible
+        if (this.cursor.tile.properties.buildable) {
+          var tower = this.add.sprite(this.cursor.x, this.cursor.y, 'tower');
+          this.cursor.tile.properties.buildable = false;
+        }
+      }
     }
+
   };
 };
